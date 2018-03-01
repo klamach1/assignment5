42beaee6bf0878b83479b1cb3e73db516e42c002
Chris Lawrence <klamach@gmail.com>
2018-03-01 08:10:10 -0500

    Initial Commit

---
+++ assignment5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment5
+//
+//  Created by Chris Lawrence on 3/1/18.
+//  Copyright © 2018 Chris Lawrence. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
